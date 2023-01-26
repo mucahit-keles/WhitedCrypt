673a04abfdf25cde8ad6d3b8964f37d0f3062b48
M6HqVBcddw2qaN4s <61710729+M6HqVBcddw2qaN4s@users.noreply.github.com>
2023-01-26 23:55:45 +0000

    Update WhitedCrypt.py

--- WhitedCrypt.py
+++ WhitedCrypt.py
@@ -1,6 +1,7 @@
 import hashlib
 
 HashingAlgorithms = {"md5": hashlib.md5, "sha1": hashlib.sha1, "sha224": hashlib.sha224, "sha256": hashlib.sha256, "sha384": hashlib.sha384, "sha512": hashlib.sha512, "sha3_224": hashlib.sha3_224, "sha3_256": hashlib.sha3_256, "sha3_384": hashlib.sha3_384, "sha3_512": hashlib.sha3_512}
+ValidMappingMethods = {1, 2}
 
 def Map(Method: int, Numbers: str):
 	Mapping = Method == 1 and "          " or Method == 2 and "0945862731"
@@ -34,7 +35,10 @@ def FromUnicodeOrder(Separator: str, UnicodeOrder: str, Mapped: bool, MappingMet
 	return Text
 
 def Encode(MappingMethod: int, PlainText: str):
-	return ToUnicodeOrder(MappingMethod == 1 and " " or MappingMethod == 2 and "1114112", PlainText, True, MappingMethod) # separator is "1114112" because a unicode can be 0x10ffff at max in python
+	if MappingMethod in ValidMappingMethods:
+		return ToUnicodeOrder(MappingMethod == 1 and " " or MappingMethod == 2 and "1114112", PlainText, True, MappingMethod) # separator is "1114112" because a unicode can be 0x10ffff at max in python
+	else:
+		return "[ERROR] You entered an invalid/unsupported MappingMethod. Supported MappingMethods are: 1 (Whitespace), 2 (Numbers)"
 
 def Decode(EncodedText: str):
 	MappingMethod = EncodedText.find(" ") != -1 and 1 or EncodedText.find("1114112") != -1 and 2
@@ -43,20 +47,26 @@ def Decode(EncodedText: str):
 	else:
 		return "[ERROR] Mapping is corrupted."
 
-def Encrypt(HashingAlgorithm, Mapped: bool, PlainText: str, Key: str):
-	HashedKey = HashingAlgorithm(Key.encode()).hexdigest()
-	MappedKey = Encode(2, HashedKey) # first parameter is 2 to map it with numbers instead of whitespaces
-	MappedText = Encode(2, PlainText) # first parameter is 2 to map it with numbers instead of whitespaces
-	EncryptedText = HashingAlgorithm.__name__.split("openssl_")[1] + "|" + str(int(MappedText) + int(MappedKey))
-	return Mapped == True and Encode(1, EncryptedText) or EncryptedText
+def Encrypt(HashingAlgorithmName: str, Mapped: bool, PlainText: str, Key: str):
+	HashingAlgorithm = HashingAlgorithms[HashingAlgorithmName.lower()]
+	if HashingAlgorithm:
+		HashedKey = HashingAlgorithm(Key.encode()).hexdigest()
+		MappedKey = Encode(2, HashedKey) # first parameter is 2 to map it with numbers instead of whitespaces
+		MappedText = Encode(2, PlainText) # first parameter is 2 to map it with numbers instead of whitespaces
+		EncryptedText = HashingAlgorithmName + "|" + str(int(MappedText) + int(MappedKey))
+		return Mapped == True and Encode(1, EncryptedText) or EncryptedText
+	else:
+		return "[ERROR] You entered an invalid/unsupported Hashing Algorithm. Supported hashing algorithms are (lightest -> heaviest): md5, sha1, sha224, sha256, sha384, sha512, sha3_224, sha3_256, sha3_384, sha3_512"
 
 def Decrypt(EncryptedText: str, Key: str):
 	EncryptedText = EncryptedText.find("|") != -1 and EncryptedText or Decode(EncryptedText)
 	HashingAlgorithm = HashingAlgorithms[EncryptedText.split("|")[0]]
-	if not HashingAlgorithm:
+	if HashingAlgorithm:
+		HashedKey = HashingAlgorithm(Key.encode()).hexdigest()
+		MappedKey = Decode(HashedKey)
+		MappedText = str(int(EncryptedText) - int(MappedKey))
+		PlainText = Decode(MappedText)
+		return PlainText
+	else:
 		return "[ERROR] Hashing Algorithm header is corrupted."
-	HashedKey = HashingAlgorithm(Key.encode()).hexdigest()
-	MappedKey = Decode(HashedKey)
-	MappedText = str(int(EncryptedText) - int(MappedKey))
-	PlainText = Decode(MappedText)
-	return PlainText
+
