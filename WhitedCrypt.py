6e33bc2bb0c0f020e59191953242df7cad28fa41
M6HqVBcddw2qaN4s <61710729+M6HqVBcddw2qaN4s@users.noreply.github.com>
2023-01-26 22:50:43 +0000

    Update WhitedCrypt.py

--- WhitedCrypt.py
+++ WhitedCrypt.py
@@ -1,4 +1,5 @@
 import hashlib
+
 HashingAlgorithms = {"md5": hashlib.md5, "sha1": hashlib.sha1, "sha224": hashlib.sha224, "sha256": hashlib.sha256, "sha384": hashlib.sha384, "sha512": hashlib.sha512, "sha3_224": hashlib.sha3_224, "sha3_256": hashlib.sha3_256, "sha3_384": hashlib.sha3_384, "sha3_512": hashlib.sha3_512}
 
 def Map(Method: int, Numbers: str):
