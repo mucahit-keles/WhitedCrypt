965c8d065e2b11dab70bb098fb9727f30526b26d
M6HqVBcddw2qaN4s <61710729+M6HqVBcddw2qaN4s@users.noreply.github.com>
2023-01-26 22:38:13 +0000

    Update WhitedCrypt.py

--- WhitedCrypt.py
+++ WhitedCrypt.py
@@ -1,4 +1,5 @@
 import hashlib
+HashingAlgorithms = {"md5": hashlib.md5, "sha1": hashlib.sha1, "sha224": hashlib.sha224, "sha256": hashlib.sha256, "sha384": hashlib.sha384, "sha512": hashlib.sha512, "sha3_224": hashlib.sha3_224, "sha3_256": hashlib.sha3_256, "sha3_384": hashlib.sha3_384, "sha3_512": hashlib.sha3_512}
 
 def Map(Method: int, Numbers: str):
 	Mapping = Method == 1 and "          " or Method == 2 and "0945862731"
@@ -37,16 +38,19 @@ def Encode(PlainText: str):
 def Decode(EncodedText: str):
 	return FromUnicodeOrder(" ", EncodedText, True, 1)
 
-def Encrypt(PlainText: str, Key: str):
-	HashedKey = hashlib.sha512(Key.encode()).hexdigest()
-	MappedKey = ToUnicodeOrder("1114112", HashedKey, True, 2) # Separator is "1114112" because a unicode can be 0x10ffff at max in python
-	MappedText = ToUnicodeOrder("1114112", PlainText, True, 2) # Last parameter is 2 to map it with numbers instead of whitespaces
-	EncryptedText = str(int(MappedText) + int(MappedKey))
-	return Encode(EncryptedText) # Encrypted text but with whitespace mapping
+def Encrypt(HashingAlgorithm, Mapped: bool, PlainText: str, Key: str):
+	HashedKey = HashingAlgorithm(Key.encode()).hexdigest()
+	MappedKey = ToUnicodeOrder("1114112", HashedKey, True, 2) # separator is "1114112" because a unicode can be 0x10ffff at max in python
+	MappedText = ToUnicodeOrder("1114112", PlainText, True, 2) # last parameter is 2 to map it with numbers instead of whitespaces
+	EncryptedText = HashingAlgorithm.__name__.split("openssl_")[1] + "|" + str(int(MappedText) + int(MappedKey))
+	return Mapped == True and Encode(EncryptedText) or EncryptedText
 
-def Decrypt(MappedEncryptedText: str, Key: str):
-	EncryptedText = Decode(MappedEncryptedText)
-	HashedKey = hashlib.sha512(Key.encode()).hexdigest()
+def Decrypt(EncryptedText: str, Key: str):
+	EncryptedText = EncryptedText.find("|") == -1 and Decode(EncryptedText) or EncryptedText
+	HashingAlgorithm = HashingAlgorithms[EncryptedText.split("|")[0]]
+	if not HashingAlgorithm:
+		return "[ERROR] Hashing Algorithm header is corrupt."
+	HashedKey = HashingAlgorithm(Key.encode()).hexdigest()
 	MappedKey = ToUnicodeOrder("1114112", HashedKey, True, 2)
 	MappedText = str(int(EncryptedText) - int(MappedKey))
 	PlainText = FromUnicodeOrder("1114112", MappedText, True, 2)
