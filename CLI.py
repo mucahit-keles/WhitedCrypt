e7453701ce5be42fb42acde4c4d73b9ee3c37947
M6HqVBcddw2qaN4s <61710729+M6HqVBcddw2qaN4s@users.noreply.github.com>
2023-01-27 01:57:31 +0000

    Update CLI.py

--- CLI.py
+++ CLI.py
@@ -1,8 +1,6 @@
 import hashlib
 import WhitedCrypt
 
-HashingAlgorithms = {"md5": hashlib.md5, "sha1": hashlib.sha1, "sha224": hashlib.sha224, "sha256": hashlib.sha256, "sha384": hashlib.sha384, "sha512": hashlib.sha512, "sha3_224": hashlib.sha3_224, "sha3_256": hashlib.sha3_256, "sha3_384": hashlib.sha3_384, "sha3_512": hashlib.sha3_512}
-
 def Start():
 	print("Press CTRL+C anytime to exit.")
 	Operation = input("Operation: ").lower()
@@ -10,8 +8,14 @@ def Start():
 	if Operation in ValidOperations:
 		Text = input("Text: ")
 		if Operation == "encode":
-			IsMapped = input("Do you want the output to be obfuscated with whitespaces? (Y/N): ").lower() == "y" and True or False
-			EncodedText = WhitedCrypt.Encode(IsMapped and 1 or 2, Text)
+			IsMapped = input("Do you want the output to be obfuscated? (Y/N): ").lower() == "y" and True or False
+			MappingMethod = 0
+			if IsMapped:
+				MappingMethod = input("Which MappingMethod do you want to use? (1/2/3): ")
+				if not (int(MappingMethod) in WhitedCrypt.ValidMappingMethods):
+					print("[ERROR] You entered an invalid/unsupported MappingMethod. Supported MappingMethods are: 1 (Whitespace), 2 (Numbers), 3 (Emojis)")
+					Start()
+			EncodedText = WhitedCrypt.Encode(IsMapped and int(MappingMethod) or 2, Text)
 			print("Encoded Text [" + str(len(EncodedText)) + "]: \"" + EncodedText + "\"")
 			Start()
 		elif Operation == "decode":
@@ -21,9 +25,15 @@ def Start():
 		elif Operation == "encrypt":
 			Key = input("Key: ")
 			HashingAlgorithm = input("Hashing Algorithm: ").lower()
-			IsMapped = input("Do you want the output to be obfuscated with whitespaces? (Y/N): ").lower() == "y" and True or False
-			if HashingAlgorithms[HashingAlgorithm]:
-				EncryptedText = WhitedCrypt.Encrypt(HashingAlgorithm, IsMapped, Text, Key)
+			IsMapped = input("Do you want the output to be obfuscated? (Y/N): ").lower() == "y" and True or False
+			MappingMethod = 0
+			if IsMapped:
+				MappingMethod = input("Which MappingMethod do you want to use? (1/2/3): ")
+				if not (int(MappingMethod) in WhitedCrypt.ValidMappingMethods):
+					print("[ERROR] You entered an invalid/unsupported MappingMethod. Supported MappingMethods are: 1 (Whitespace), 2 (Numbers), 3 (Emojis)")
+					Start()
+			if WhitedCrypt.HashingAlgorithms[HashingAlgorithm]:
+				EncryptedText = WhitedCrypt.Encrypt(HashingAlgorithm, IsMapped, int(MappingMethod), Text, Key)
 				print("Encrypted Text [" + str(len(EncryptedText)) + "]: \"" + EncryptedText + "\"")
 				Start()
 			else:
