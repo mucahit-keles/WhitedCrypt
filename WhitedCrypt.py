7a77780e0c85146b952989ad983d4e99494a741e
mucahit-keles <131585878+mucahit-keles@users.noreply.github.com>
2023-06-02 00:36:46 +0300

    Buglar düzeltildi

--- WhitedCrypt.py
+++ WhitedCrypt.py
@@ -1,3 +1,4 @@
+import sys
 import hashlib as HashKutuphanesi
 
 HashlemeAlgoritmalari = {"md5": HashKutuphanesi.md5, "sha1": HashKutuphanesi.sha1, "sha224": HashKutuphanesi.sha224, "sha256": HashKutuphanesi.sha256, "sha384": HashKutuphanesi.sha384, "sha512": HashKutuphanesi.sha512, "sha3_224": HashKutuphanesi.sha3_224, "sha3_256": HashKutuphanesi.sha3_256, "sha3_384": HashKutuphanesi.sha3_384, "sha3_512": HashKutuphanesi.sha3_512}
@@ -48,9 +49,9 @@ def KodlamayiCoz(KodlanmisYazi: str, SifrelemeCozmekIcin: bool = False):
 		return SifrelemeCozmekIcin == True and "[HATA] Geçersiz bir şifre girdiniz." or "[HATA] Maskeleme bozuk."
 
 def Sifrele(HashlemeAlgoritmasiAdi: str, Maskeli: bool, MaskelemeMetodu: int, DuzYazi: str, Anahtar: str):
-	HashlemeAlgoritmasi = HashlemeAlgoritmalari[HashlemeAlgoritmasiAdi.lower()]
-	if HashlemeAlgoritmasi:
-		if MaskelemeMetodu in GecerliMaskelemeMetodlari:
+	if HashlemeAlgoritmasiAdi in HashlemeAlgoritmalari:
+		HashlemeAlgoritmasi = HashlemeAlgoritmalari[HashlemeAlgoritmasiAdi.lower()]
+		if Maskeli == False or MaskelemeMetodu in GecerliMaskelemeMetodlari:
 			HashliAnahtar = HashlemeAlgoritmasi(Anahtar.encode()).hexdigest()
 			KodlanmisHashliAnahtar = Kodla(2, HashliAnahtar) # İlk parametrenin 2 olmasının sebebi sayılarla maskeleme yapmak istememiz
 			KodlanmisYazi = Kodla(2, DuzYazi) # İlk parametrenin 2 olmasının sebebi sayılarla maskeleme yapmak istememiz
@@ -73,3 +74,28 @@ def SifrelemeyiCoz(SifrelenmisYazi: str, Anahtar: str):
 		return CozulmusYazi
 	else:
 		return "[HATA] Hashleme Algoritması header'ı bozuk."
+
+
+Parametreler = sys.argv
+
+if len(Parametreler) > 1:
+	Operasyon = Parametreler[1].lower()
+	if Operasyon == "kodla":
+		MaskelemeMetodu = int(Parametreler[2])
+		DuzYazi = Parametreler[3]
+		print(Kodla(MaskelemeMetodu, DuzYazi))
+	elif Operasyon == "kodlamayicoz":
+		KodlanmisYazi = Parametreler[2]
+		print(KodlamayiCoz(KodlanmisYazi))
+	elif Operasyon == "sifrele":
+		HashlemeAlgoritmasiAdi = Parametreler[2]
+		Maskeli = Parametreler[3].lower() == "true"
+		MaskelemeMetodu = int(Parametreler[4])
+		DuzYazi = Parametreler[5]
+		Anahtar = Parametreler[6]
+		print(Sifrele(HashlemeAlgoritmasiAdi, Maskeli, MaskelemeMetodu, DuzYazi, Anahtar))
+	elif Operasyon == "sifrelemeyicoz":
+		SifrelenmisYazi = Parametreler[2]
+		Anahtar = Parametreler[3]
+		print(SifrelemeyiCoz(SifrelenmisYazi, Anahtar))
+
